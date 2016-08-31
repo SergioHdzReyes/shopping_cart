cde6a171f591122ffd9d58c5d2d57a7ec0f3ed82
FredyReyes93 <fredy_reyes93@hotmail.com>
2016-08-30 23:33:16 -0500

    Menu

--- index.php
+++ index.php
@@ -3,14 +3,48 @@
 <html>
 <head>
     <title>Carrito de compras</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
 </head>
 <body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand">Shopping Cart
+            </a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="">Inicio</a></li>
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="">Contacto <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+
+                    </ul>
+                </li>
+                <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="">Ayuda<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+
+                    </ul>
+                </li>
+
+                <li><a href="">Carro de compras</a> </li>
+            </ul>
+        </div>
+    </div>
+</nav>
     <h1>Hello World!</h1>
     <?php
     $cart = new cart();
     var_dump($cart);
     echo 'test';
     ?>
+
 <h2>EL gallo de Baez</h2>
 </body>
 </html>
