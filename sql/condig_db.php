b09cb534eda8b9bb67ce370f8b6e635a9b3a8637
Sergio Hernandez Reyes <sergioreyes_90290@hotmail.com>
2016-08-31 22:58:22 -0500

    Archivo de configuracion de MySQL

---
+++ sql/condig_db.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sergio Hdz.
+ * Date: 31/08/16
+ * Time: 10:45 PM
+ */
+
+define('HOSTNAME', 'localhost');
+define('DATABASE', 'basedatos');
+define('USER', 'user');
+define('PASSWORD', 'pass');
