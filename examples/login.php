3a01dbafd63b424fd9472703792149a39bcdf8b4
FredyReyes93 <fredy_reyes93@hotmail.com>
2016-09-04 14:53:16 -0500

    Login

--- examples/login.php
+++ examples/login.php
@@ -5,3 +5,29 @@
  * Date: 01/09/2016
  * Time: 12:11 AM
  */
+?>
+
+<html>
+<head>
+    <title>Ingresar</title>
+    <meta name="viewport" content="width=device-width, initial-escale=1.0">
+    <meta http-equiv="Content-Type" content="text/html ; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+
+</head>
+
+<body>
+
+<div class="jumbotron login">
+    <form method="POST" name="login" id="login">
+        <label>Usuario</label>
+        <input type="text" name="username" id="username" class="form-control">
+        <label>Contraseña</label>
+        <input type="text" name="password" id="password" class="form-control">
+        <input type="submit" class="btn btn-primary" value="Ingresar">
+    </form>
+</div>
+</body>
+</html>
+
