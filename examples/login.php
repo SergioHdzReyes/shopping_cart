7bed98ee1e36e986b899708b95ed50e329fb5ea5
checo1990 <checoreyes_90290@hotmail.com>
2016-09-06 23:45:51 -0500

    Modificaciones a estructura de login

--- examples/login.php
+++ examples/login.php
@@ -7,27 +7,33 @@
  */
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
     <title>Ingresar</title>
     <meta name="viewport" content="width=device-width, initial-escale=1.0">
     <meta http-equiv="Content-Type" content="text/html ; charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../css/login.css">
-
 </head>
-
 <body>
 
-<div class="jumbotron login">
+<div class="login-centered">
     <form method="POST" name="login" id="login">
-        <label>Usuario</label>
-        <input type="text" name="username" id="username" class="form-control">
-        <label>Contraseña</label>
-        <input type="text" name="password" id="password" class="form-control">
-        <input type="submit" class="btn btn-primary" value="Ingresar">
+        <div class="form-group">
+            <label>Usuario</label>
+            <input name="username" id="username" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <label>Contraseña</label>
+            <input name="password" id="password" class="form-control" type="text">
+        </div>
+        <div class="form-group">
+            <input class="btn btn-primary boton-login" value="Ingresar" type="submit">
+        </div>
     </form>
 </div>
+
 </body>
 </html>
 
