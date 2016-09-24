affe154eb4ac2072a80b16ec808f516f93925292
Sergio Hernández Reyes <sergio.hdzreyes@gmail.com>
2016-09-24 00:32:57 +0000

    Se reacomodo estructura del sistema, se dio funcionalidad de un framework

--- examples/home.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Sergio Hdz.
- * Date: 31/08/2016
- * Time: 03:32 AM
- */
---
+++ web/front.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing;
+
+$request = Request::createFromGlobals();
+$routes = require __DIR__.'/../src/app.php';
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+d($matcher);
+try{
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+    
+    $response = new Response(ob_get_clean());
+}catch(Routing\Exception\ResourceNotFoundException $e){
+    $response = new Response('Not Found', 404);
+}catch(Exception $e){
+    $response = new Response('An error ocurred', 500);
+}
+$response->send();
+?>
